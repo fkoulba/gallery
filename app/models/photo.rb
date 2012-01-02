128509509557b1484fe0d59b75d291c166fcf754
Fyodor Koulba <fyodor@koulba.net>
2012-01-02 21:21:29 +0400

    Tuned up photos list

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,17 +1,39 @@
 class Photo
 
   def self.web_path
-    'photos'
+    '/photos'
+  end
+
+  def self.root_path
+    File.join(Rails.root, 'public', self.web_path)
   end
 
   def self.scan
-    Dir.glob(File.join(Rails.root, 'public', self.web_path, '*.{jpg,JPG}'))
+    Dir.glob(File.join(self.root_path, '*.{jpg,JPG}')).collect do |filename|
+      self.init_with_filename(File.basename(filename))
+    end
+  end
+
+  def self.init_with_filename(filename)
+    photo = self.new
+    photo.filename = filename
+    photo
   end
 
   def self.upload(upload)
     name = File.basename(upload.original_filename)
-    path = File.join(Rails.root, 'public', self.web_path, name)
+    path = File.join(self.root_path, name)
     File.open(path, "wb") { |f| f.write(upload.read) }
+  end
+
+  attr_accessor :filename
+
+  def web_path
+    File.join(self.class.web_path, filename)
+  end
+
+  def root_path
+    File.join(self.class.root_path, filename)
   end
 
 end
