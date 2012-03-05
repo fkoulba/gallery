4c736f170370684172e0f1de6a7ab6c46a54ab2b
Fyodor Koulba <fyodor@koulba.net>
2012-03-06 00:17:09 +0400

    Upload JS rewritten to jQuery

--- app/assets/javascripts/upload_photos.js
+++ app/assets/javascripts/upload_photos.js
@@ -1,38 +1,38 @@
 $(function () {
   (function () {
-    var filesUpload = document.getElementById("files-upload"),
-      dropArea = document.getElementById("drop-area"),
-      fileList = document.getElementById("file-list");
+    var filesUpload = $('#files-upload'),
+      dropArea = $('#drop-area'),
+      fileList = $('#file-list');
 
     function uploadFile(file) {
-      var li = document.createElement("li"),
-        div = document.createElement("div"),
+      var li = $('<li class="photo_thumb" />'),
+        div = $('<div />'),
         img,
-        progressBarContainer = document.createElement("div"),
-        progressBar = document.createElement("div"),
+        progressBarContainer = $('<div />'),
+        progressBar = $('<div />'),
         reader,
         xhr,
         fileInfo;
 
-      li.appendChild(div);
+      li.append(div);
 
-      progressBarContainer.className = "progress-bar-container";
-      progressBar.className = "progress-bar";
-      progressBarContainer.appendChild(progressBar);
-      li.appendChild(progressBarContainer);
+      progressBarContainer.addClass('progress-bar-container');
+      progressBar.addClass('progress-bar');
+      progressBarContainer.append(progressBar);
+      li.append(progressBarContainer);
 
       /*
         If the file is an image and the web browser supports FileReader,
         present a preview in the file list
       */
-      if (typeof FileReader !== "undefined" && (/image/i).test(file.type)) {
-        img = document.createElement("img");
-        li.appendChild(img);
+      if (typeof FileReader !== 'undefined' && (/image/i).test(file.type)) {
+        img = $('<img />');
+        li.append(img);
         reader = new FileReader();
         reader.onload = (function (theImg) {
-          return function (evt) {
-            theImg.src = evt.target.result;
-            theImg.width = 100;
+          return function (event) {
+            theImg.attr('src', event.target.result);
+            theImg.attr('width', 100);
           };
         }(img));
         reader.readAsDataURL(file);
@@ -42,9 +42,9 @@ $(function () {
       xhr = new XMLHttpRequest();
 
       // Update progress bar
-      xhr.upload.addEventListener("progress", function (evt) {
-        if (evt.lengthComputable) {
-          progressBar.style.width = (evt.loaded / evt.total) * 100 + "%";
+      xhr.upload.addEventListener('progress', function (event) {
+        if (event.lengthComputable) {
+          progressBar.css('width', (event.loaded / event.total) * 100 + '%');
         }
         else {
           // No data to calculate on
@@ -52,73 +52,71 @@ $(function () {
       }, false);
 
       // File uploaded
-      xhr.addEventListener("load", function () {
-        progressBarContainer.className += " uploaded";
-        progressBar.innerHTML = "Uploaded!";
+      xhr.addEventListener('load', function () {
+        progressBarContainer.addClass('uploaded');
+        progressBar.html('Uploaded!');
       }, false);
 
-      xhr.open("post", "upload/upload.php", true);
+      xhr.open('post', 'upload/upload.php', true);
 
       // Set appropriate headers
-      xhr.setRequestHeader("Content-Type", "multipart/form-data");
-      xhr.setRequestHeader("X-File-Name", file.fileName);
-      xhr.setRequestHeader("X-File-Size", file.fileSize);
-      xhr.setRequestHeader("X-File-Type", file.type);
+      xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+      xhr.setRequestHeader('X-File-Name', file.fileName);
+      xhr.setRequestHeader('X-File-Size', file.fileSize);
+      xhr.setRequestHeader('X-File-Type', file.type);
 
       // Send the file (doh)
       xhr.send(file);*/
 
       // Present file info and append it to the list of files
-      fileInfo = "<div><strong>Name:</strong> " + file.name + "</div>";
-      fileInfo += "<div><strong>Size:</strong> " + parseInt(file.size / 1024, 10) + " kb</div>";
-      fileInfo += "<div><strong>Type:</strong> " + file.type + "</div>";
+      fileInfo = '<div><strong>Name:</strong> ' + file.name + '</div>';
+      fileInfo += '<div><strong>Size:</strong> ' + parseInt(file.size / 1024, 10) + ' kb</div>';
+      fileInfo += '<div><strong>Type:</strong> ' + file.type + '</div>';
       div.innerHTML = fileInfo;
 
-      fileList.appendChild(li);
+      fileList.append(li);
     }
 
     function traverseFiles(files) {
-      if (typeof files !== "undefined") {
+      if (typeof files !== 'undefined') {
         for (var i=0, l=files.length; i<l; i++) {
           uploadFile(files[i]);
         }
       }
       else {
-        fileList.innerHTML = "No support for the File API in this web browser";
+        fileList.innerHTML = 'No support for the File API in this web browser';
       }
     }
 
-    filesUpload.addEventListener("change", function () {
+    filesUpload.bind('change', function () {
       traverseFiles(this.files);
-    }, false);
+    });
 
-    dropArea.addEventListener("dragleave", function (evt) {
-      var target = evt.target;
+    dropArea.bind({
+      dragleave: function (event) {
+        var target = event.target;
 
-      if (target && target === dropArea) {
-        this.className = "";
+        if (target && target === dropArea) {
+          $(this).removeClass('over');
+        }
+        return false;
+      },
+
+      dragenter: function (event) {
+        $(this).addClass('over');
+        return false;
+      },
+
+      dragover: function (event) {
+        return false;
+      },
+
+      drop: function (event) {
+        traverseFiles(event.originalEvent.dataTransfer.files);
+        $(this).removeClass('over');
+        return false;
       }
-      evt.preventDefault();
-      evt.stopPropagation();
-    }, false);
-
-    dropArea.addEventListener("dragenter", function (evt) {
-      this.className = "over";
-      evt.preventDefault();
-      evt.stopPropagation();
-    }, false);
-
-    dropArea.addEventListener("dragover", function (evt) {
-      evt.preventDefault();
-      evt.stopPropagation();
-    }, false);
-
-    dropArea.addEventListener("drop", function (evt) {
-      traverseFiles(evt.dataTransfer.files);
-      this.className = "";
-      evt.preventDefault();
-      evt.stopPropagation();
-    }, false);
+    });
   })();
 });
 
