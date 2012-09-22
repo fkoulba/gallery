5c53cbdd9a46ad51c793b1418c72421979c606c7
Fyodor Koulba <fyodor@koulba.net>
2012-09-22 16:05:46 +0400

    Selectable and drag-n-droppable experiment branched

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,50 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+  /*$('.thumbs_list .thumb a').click(function(e) {
+    console.log('click', e);
+    if (e.ctrlKey) {
+        $(this).parents('.thumb').toggleClass('selected');
+    } else {
+        $('.thumbs_list .thumb').removeClass('selected');
+    }
+    return false;
+  });*/
+
+  /*$('.thumbs_list .thumb a').dblclick(function(e) {
+    console.log('dblclick');
+    window.location = $(this).attr('href');
+    return false;
+  });*/
+
+  $('.thumbs_list').selectable();
+
+  $('.tags_list .tag').droppable({
+    drop: function(event, ui) {
+      console.log('The square with ID "' + ui.draggable.data('photo_id') + '" was dropped onto me!');
+      ui.draggable.draggable('disable');
+      $(this).droppable('disable');
+      ui.draggable.draggable('option', 'revert', false);
+      ui.draggable.find('.tags').html($(this).html());
+    }
+  });
+
+  $('.thumbs_list .thumb').draggable({
+    containment: 'document',
+    //helper: 'clone',
+    helper: function() {
+      var selected = $('.thumbs_list .thumb.selected');
+      if (selected.length === 0) {
+        selected = $(this);
+      }
+      var container = $('<ul />').attr('id', 'draggingContainer');
+      container.append(selected.clone());
+      return container;
+    },
+    cursor: 'move',
+    revert: true
+  });
+});
+
